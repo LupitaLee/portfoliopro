7da346b8831044e44ffb9aba857fec78bbcd295c
Lupita <lupitarivera8899@gmail.com>
2022-01-28 21:28:34 -0600

    add classNames to Home component

--- client/src/PortfolioContainer/Home/Home.jsx
+++ client/src/PortfolioContainer/Home/Home.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Home = () => {
-  return <div></div>;
+  return <div className="home-container">
+            <div className="home-parent">
+                <div className="home-details">
+                    <div className="colz">
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+         </div>;
 };
 
 export default Home;
